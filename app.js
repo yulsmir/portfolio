13fab914281df02a5dc4ab3f830b5afa6938ab65
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-04-24 21:02:59 +0200

    Add background image, define colors

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const burgerIcon = document.querySelector('.burger-icon');
+const navLinks = document.querySelector('nav ul');
+
+burgerIcon.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
