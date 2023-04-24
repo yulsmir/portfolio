483337df649ab0bb007d64b54715a9c53cf664f7
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-04-24 23:29:44 +0200

    Add favicon, fix footer

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
 'use strict';
-
-const burgerIcon = document.querySelector('.burger-icon');
-const navLinks = document.querySelector('nav ul');
-
-burgerIcon.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-});
