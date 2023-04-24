5fdd07d964e51400d499dbbba204a39c11e5e637
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-04-25 01:32:42 +0200

    Add project setup

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const container = document.querySelector('.container');
+
+document.querySelectorAll('.project').forEach((project) => {
+  project.addEventListener('click', () => {
+    // Set container div to white
+    // container.style.backgroundColor = '#fff';
+    container.innerHTML = 'Hohoh';
+  });
+});
