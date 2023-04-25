dfca5ddf15f9eab77bac0ddc77143a963ac122f9
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-04-25 04:30:40 +0200

    Fix favicon, add nav bar click handler (hide/unhide section)

--- app.js
+++ app.js
@@ -13,3 +13,28 @@ const displayProject1 = () => {
 };
 
 project1.addEventListener('click', displayProject1);
+
+// Hide/unhide corresponding section
+const navLinks = document.querySelectorAll('#menu a');
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const sectionId = link.getAttribute('href');
+    const section = document.querySelector(sectionId);
+
+    console.log(section);
+
+    if (section) {
+      const sections = document.querySelectorAll('main section');
+
+      sections.forEach((s) => {
+        if (s !== section) {
+          s.classList.add('hidden');
+        }
+      });
+      section.classList.remove('hidden');
+    }
+  });
+});
