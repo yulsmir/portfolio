ad3c9b2429410333ef0d3c2630a748b58be7fa67
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-04-26 05:11:38 +0200

    Add post request

--- app.js
+++ app.js
@@ -69,84 +69,6 @@ fetch('http://my-json-server.typicode.com/yulsmir/test-server/books')
     });
   });
 
-// Create a new book
-// const newBook = {
-//   title: 'New Book Title',
-//   author: 'New Book Author',
-//   language: 'New Language',
-// };
-
-// // Function to add a new book to the server and update the book list
-// const bookList = document.querySelector('.book-list');
-// const addBookForm = document.getElementById('add-book-form');
-
-// // Function to display the list of books on the page
-// function displayBooks(books) {
-//   bookList.innerHTML = '';
-//   books.forEach((book) => {
-//     const bookDiv = document.createElement('div');
-//     bookDiv.className = 'book';
-//     const coverPicture = document.createElement('picture');
-//     coverPicture.className = 'cover';
-
-//     const coverImage = document.createElement('img');
-//     coverImage.src = book.cover;
-//     coverPicture.appendChild(coverImage);
-
-//     const title = document.createElement('h3');
-//     title.textContent = book.title;
-
-//     const author = document.createElement('p');
-//     author.textContent = book.author;
-
-//     const language = document.createElement('p');
-//     author.textContent = book.language;
-
-//     bookDiv.appendChild(coverPicture);
-//     bookDiv.appendChild(title);
-//     bookDiv.appendChild(author);
-//     bookDiv.appendChild(language);
-
-//     bookList.appendChild(bookDiv);
-//   });
-// }
-
-// Function to add a new book to the server and update the book list
-// function addBook(event) {
-//   event.preventDefault();
-
-//   const titleInput = document.getElementById('title-input');
-//   const authorInput = document.getElementById('author-input');
-//   const languageInput = document.getElementById('language-input');
-
-//   const newBook = {
-//     title: titleInput.value,
-//     author: authorInput.value,
-//     language: languageInput.value,
-//   };
-
-//   fetch('http://my-json-server.typicode.com/yulsmir/test-server/books', {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify(newBook),
-//   })
-//     .then((response) => response.json())
-//     .then((books) => {
-//       displayBooks(books);
-//       titleInput.value = '';
-//       authorInput.value = '';
-//       languageInput.value = '';
-//     })
-//     .catch((error) => {
-//       console.error('Error:', error);
-//     });
-// }
-
-// Attach event listener to the form submit button
-// addBookForm.addEventListener('submit', addBook);
-
 const form = document.getElementById('add-book-form');
 
 const addBook = (e) => {
@@ -155,6 +77,7 @@ const addBook = (e) => {
   const titleInput = document.getElementById('title-input');
   const authorInput = document.getElementById('author-input');
   const languageInput = document.getElementById('language-input');
+  const coverInput = document.getElementById('cover-input');
 
   fetch('http://my-json-server.typicode.com/yulsmir/test-server/books', {
     method: 'POST',
@@ -162,13 +85,11 @@ const addBook = (e) => {
       title: titleInput.value,
       author: authorInput.value,
       language: languageInput.value,
+      cover: coverInput.value,
     }),
 
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
-      'Access-Control-Max-Age': '2592000',
     },
   })
     .then((response) => response.json())
@@ -176,7 +97,37 @@ const addBook = (e) => {
       console.log(json);
       const booksList = document.querySelector('.book-list');
 
-      booksList.innerHTML = `Title: ${json.title}, author: ${json.author}, language: ${json.language}`;
+      const newBookDiv = document.createElement('div');
+      newBookDiv.className = 'book';
+
+      const coverPicture = document.createElement('picture');
+      coverPicture.className = 'cover';
+
+      const coverImage = document.createElement('img');
+      coverImage.src = json.cover;
+      coverPicture.appendChild(coverImage);
+
+      const title = document.createElement('p');
+      title.textContent = `Title: ${json.title}`;
+
+      const language = document.createElement('p');
+      language.textContent = `Language: ${json.language}`;
+
+      const author = document.createElement('p');
+      author.textContent = `Author: ${json.author}`;
+
+      const checkPageBtn = document.createElement('input');
+      checkPageBtn.type = 'button';
+      checkPageBtn.className = 'btn btn-round';
+      checkPageBtn.value = 'Check page';
+
+      newBookDiv.appendChild(coverPicture);
+      newBookDiv.appendChild(title);
+      newBookDiv.appendChild(language);
+      newBookDiv.appendChild(author);
+      newBookDiv.appendChild(checkPageBtn);
+
+      booksList.appendChild(newBookDiv);
     });
 };
 
