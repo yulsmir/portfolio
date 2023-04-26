c222fad696db21f185981117594e471d9eeb0b32
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-04-26 03:36:10 +0200

    Add books

--- app.js
+++ app.js
@@ -1,19 +1,5 @@
 'use strict';
 
-const project1 = document.querySelector('.project-1');
-const container = document.querySelector('.container');
-
-const displayProject1 = () => {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', './projects/project-01.html');
-  xhr.onload = function () {
-    container.innerHTML = xhr.response;
-  };
-  xhr.send();
-};
-
-project1.addEventListener('click', displayProject1);
-
 // Hide/unhide corresponding section
 const navLinks = document.querySelectorAll('#menu a');
 
